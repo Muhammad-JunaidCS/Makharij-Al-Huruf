28b6e03fb2e84a089f6487c4ceb3919270f9e968
MUHAMMAD JUNAID <bcsf18m010@pucit.edu.pk>
2021-11-07 18:44:28 +0500

    learning halqiyah added

--- app/src/main/java/com/example/makhrijal_hurf/LearningActivity.java
+++ app/src/main/java/com/example/makhrijal_hurf/LearningActivity.java
@@ -2,13 +2,26 @@ package com.example.makhrijal_hurf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class LearningActivity extends AppCompatActivity {
-
+public class LearningActivity extends AppCompatActivity implements View.OnClickListener{
+    Button halqiyahButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learning);
+        halqiyahButton=findViewById(R.id.halqiyah);
+        halqiyahButton.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.halqiyah)
+        {
+            Intent intent =new Intent(this,HalqiyahActivity.class);
+            startActivity(intent);
+        }
     }
 }
