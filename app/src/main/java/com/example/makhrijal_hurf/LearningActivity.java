91cb8f60024038991e1f5451a42db0df3af0b209
MUHAMMAD JUNAID <bcsf18m010@pucit.edu.pk>
2021-11-07 20:56:34 +0500

    learning Shajariyah-Haafiyah added

--- app/src/main/java/com/example/makhrijal_hurf/LearningActivity.java
+++ app/src/main/java/com/example/makhrijal_hurf/LearningActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class LearningActivity extends AppCompatActivity implements View.OnClickListener{
-    Button halqiyahButton,lahatiyahButton;
+    Button halqiyahButton,lahatiyahButton,haafiyahButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,18 +17,28 @@ public class LearningActivity extends AppCompatActivity implements View.OnClickL
         halqiyahButton.setOnClickListener(this);
         lahatiyahButton=findViewById(R.id.lahatiyah);
         lahatiyahButton.setOnClickListener(this);
+        haafiyahButton=findViewById(R.id.haafiyah);
+        haafiyahButton.setOnClickListener(this);
     }
     @Override
     public void onClick(View v) {
-        if(v.getId()==R.id.halqiyah)
-        {
-            Intent intent =new Intent(this,HalqiyahActivity.class);
-            startActivity(intent);
-        }
-        if(v.getId()==R.id.lahatiyah)
-        {
-            Intent intent =new Intent(this,LahatiyahActivity.class);
-            startActivity(intent);
+        Intent intent;
+        switch (v.getId()) {
+            case R.id.halqiyah:
+                intent = new Intent(this, HalqiyahActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.lahatiyah:
+                intent = new Intent(this, LahatiyahActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.haafiyah:
+                intent=new Intent(this,Shajariyah_HaafiyahActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + v.getId());
         }
     }
 }
---
+++ app/src/main/java/com/example/makhrijal_hurf/Shajariyah_HaafiyahActivity.java
@@ -0,0 +1,14 @@
+package com.example.makhrijal_hurf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Shajariyah_HaafiyahActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shajariyah_haafiyah);
+    }
+}
